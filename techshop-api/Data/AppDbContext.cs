90af099719d01640cf90b0d02359786cbc7bed79
nicolasallp <nikolasalp@gmail.com>
2025-01-18 12:47:30 -0300

    add: db context

--- techshop-api/Data/AppDbContext.cs
+++ techshop-api/Data/AppDbContext.cs
@@ -11,5 +11,10 @@ namespace techshop_api.Data
         }
 
         public DbSet<Admin> Admins { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Review> Reviews { get; set; }
     }
 }
