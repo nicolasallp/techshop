8fef8a5bb2379067f11a7b9d955f5c8fec995996
nicolasallp <nikolasalp@gmail.com>
2025-01-19 13:14:46 -0300

    add: models

---
+++ techshop_admin/Models/Entities/Admin.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace techshop_admin.Models.Entities
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+
+        [JsonProperty("email")]
+        public string? Email { get; set; }
+
+        [JsonProperty("password")]
+        public string? Password { get; set; }
+    }
+}
---
+++ techshop_admin/Models/ViewModels/AccessViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace techshop_admin.Models.ViewModels
+{
+    public class AccessViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide the email.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide the password.")]
+        public string? Password { get; set; }
+    }
+}
