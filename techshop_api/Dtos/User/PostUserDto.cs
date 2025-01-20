e310f4d2442c69de2d72decd6f8794da242f77e7
nicolasallp <nikolasalp@gmail.com>
2025-01-20 20:59:57 -0300

    add: api dtos

---
+++ techshop_api/Dtos/User/PostUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace techshop_api.Dtos.User
+{
+    public class PostUserDto
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+    }
+}
