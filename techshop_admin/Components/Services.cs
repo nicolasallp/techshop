cebf263fb1e33a059e0f832603a1c40eb88aba0f
nicolasallp <nikolasalp@gmail.com>
2025-01-20 21:05:48 -0300

    add: admin services

---
+++ techshop_admin/Components/Services.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Reflection;
+using System.Text;
+
+namespace techshop_admin.Components
+{
+    public class Services
+    {
+        static HttpClient HttpClient = new();
+
+        public static async Task<List<T>?> GetData<T>(string url)
+        {
+            HttpResponseMessage response = await HttpClient.GetAsync(url);
+            string responseData = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<T>>(responseData);
+        }
+
+        public static async Task PostData(string url, string data)
+        {
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            var response = await HttpClient.PostAsync(url, content);
+        }
+    }
+}
