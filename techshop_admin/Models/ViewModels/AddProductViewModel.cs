de894804c0c159f68788e6654515059a4621e43d
nicolasallp <nikolasalp@gmail.com>
2025-01-20 21:05:21 -0300

    update: models

---
+++ techshop_admin/Models/ViewModels/AddProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace techshop_admin.Models.ViewModels
+{
+    public class AddProductViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Provide the product name.")]
+        public string? Name { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Provide the product description.")]
+        public string? Description { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Provide the product price.")]
+        public string? Price { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Provide the product brand.")]
+        public string? Brand { get; set; }
+        public int Availability { get; set; }
+    }
+}
