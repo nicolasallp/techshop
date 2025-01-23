909f58b8d9d2bcaa36379780adba64e6af72078b
nicolasallp <nikolasalp@gmail.com>
2025-01-23 12:41:44 -0300

    update: product entity model

--- techshop/Models/Entities/Product.cs
+++ techshop/Models/Entities/Product.cs
@@ -7,7 +7,7 @@
         public string? Description { get; set; }
         public decimal Price { get; set; }
         public string? Brand { get; set; }
-        public byte[]? Image { get; set; }
+        public string? Image { get; set; }
         public int Availability { get; set; }
     }
 }
