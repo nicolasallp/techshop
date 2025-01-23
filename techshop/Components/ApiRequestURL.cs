3e542059e03648a90c617dcfb912bc595857a4cf
nicolasallp <nikolasalp@gmail.com>
2025-01-23 12:42:23 -0300

    add: component

---
+++ techshop/Components/ApiRequestURL.cs
@@ -0,0 +1,11 @@
+﻿namespace techshop.Components
+{
+    public static class ApiRequestURL
+    {
+        public static readonly string Users = "https://localhost:5280/api/Users";
+        public static readonly string Products = "https://localhost:5280/api/Products";
+        public static readonly string Orders = "https://localhost:5280/api/Orders";
+        public static readonly string Reviews = "https://localhost:5280/api/Reviews";
+        public static readonly string Resources = "https://localhost:5280/Resources";
+    }
+}
