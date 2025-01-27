19722e0c7b38f3098c0da5c73748f42f1181adba
nicolasallp <nikolasalp@gmail.com>
2025-01-27 13:10:37 -0300

    update: cart model

--- techshop/Models/Entities/Cart.cs
+++ techshop/Models/Entities/Cart.cs
@@ -1,9 +1,13 @@
-﻿namespace techshop.Models.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace techshop.Models.Entities
 {
     public class Cart
     {
-        public int Id { get; set; }
+        public int UserId { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
+        public User? User { get; set; }
+        public Product? Product { get; set; }
     }
 }
