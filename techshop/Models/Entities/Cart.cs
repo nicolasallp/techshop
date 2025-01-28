c6d614c19de955ed9cd98d48f87c00317b07d2ab
nicolasallp <nikolasalp@gmail.com>
2025-01-27 21:00:51 -0300

    update: cart management

--- techshop/Models/Entities/Cart.cs
+++ techshop/Models/Entities/Cart.cs
@@ -4,6 +4,7 @@ namespace techshop.Models.Entities
 {
     public class Cart
     {
+        public int Id { get; set; }
         public int UserId { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
