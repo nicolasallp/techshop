35373aaf3ee2701c7504f90688060bf119af9efc
Nicolas <Nicolas@LAPTOP-O902233L>
2025-06-30 18:40:22 -0300

    update: cart state global state

---
+++ techshop/DataManager/CartState.cs
@@ -0,0 +1,12 @@
+﻿namespace techshop.DataManager
+{
+    public class CartState
+    {
+        public event Action<int>? OnChange;
+
+        public void SendCount(int count)
+        {
+            OnChange?.Invoke(count);
+        }
+    }
+}
