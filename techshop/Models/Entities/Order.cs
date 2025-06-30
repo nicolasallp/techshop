f4408447c12200fd070e91328916cf16d31e362e
Nicolas <Nicolas@LAPTOP-O902233L>
2025-06-30 18:41:11 -0300

    update: order entity

--- techshop/Models/Entities/Order.cs
+++ techshop/Models/Entities/Order.cs
@@ -2,8 +2,9 @@
 {
     public class Order
     {
-        public string? Id { get; set; }
-        public decimal Fee { get; set; }
+        public string? ProductId { get; set; }
+        public string? UserId { get; set; }
+        public int Quantity { get; set; }
         public string? Status { get; set; }
     }
 }
