9c61649a4e3dea14ff3a5f9d0634dbd532d28036
nicolasallp <nikolasalp@gmail.com>
2025-07-03 13:26:33 -0300

    update: misc

--- techshop/DataManager/ApiRequest.cs
+++ techshop/DataManager/ApiRequest.cs
@@ -17,14 +17,14 @@ namespace techshop.DataManager
             return JsonConvert.DeserializeObject<List<T>>(responseData);
         }
 
-        public static async Task<List<T>?> GetData<T>(string url, string userId)
+        public static async Task<List<T>?> GetData<T>(string url, string? userId)
         {
             HttpResponseMessage response = await HttpClient.GetAsync($"{url}/ByUser/{userId}");
             string responseData = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<List<T>>(responseData);
         }
 
-        public static async Task<T?> GetDataById<T>(string url, string id)
+        public static async Task<T?> GetDataById<T>(string url, string? id)
         {
             HttpResponseMessage response = await HttpClient.GetAsync($"{url}/{id}");
             string responseData = await response.Content.ReadAsStringAsync();
--- techshop/Models/Entities/Review.cs
+++ techshop/Models/Entities/Review.cs
@@ -3,6 +3,7 @@
     public class Review
     {
         public string? Id { get; set; }
+        public string? ProductId { get; set; }
         public int Rating { get; set; }
         public string? ReviewText { get; set; }
         public DateTime? CreatedDate { get; set; } = DateTime.Now;
